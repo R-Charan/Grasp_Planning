eb8e89b8ce374255465f1e715797c43d2d658d14
R-Charan <R-Charan>
2023-11-14 16:17:53 +0530

    Grasp Planning with STL file

--- gripper/scripts/grasp.py
+++ gripper/scripts/grasp.py
@@ -7,23 +7,29 @@ from geometry_msgs.msg import Pose
 from transformation import object_world_transformation
 
 epsilon_quality = []
-pose = Pose()
+object_pose = Pose()
 
 # Pose at which the graspable body will be imported onto to the GraspIt! scene.
-pose.position.x = 0.0454457
-pose.position.y = 0.0111662
-pose.position.z = 0.107
-pose.orientation.x = 0
-pose.orientation.y = 0
-pose.orientation.z = 0
-pose.orientation.w = 1
+object_pose.position.x = 0.0
+object_pose.position.y = 0.0
+object_pose.position.z = 0
+object_pose.orientation.x = 0
+object_pose.orientation.y = 0
+object_pose.orientation.z = 0
+object_pose.orientation.w = 1
+
 
 def grasp():
-	# Set up GraspIt! scene and planner by importing the gripper and the graspable body to a desired pose.
-	# TODO: Combine table and gripper import locations as a single world file.
+
+	# Clear Graspit World and import the world file that contains the table and the gripper
 	GraspitCommander.clearWorld()
-	GraspitCommander.importRobot("RobotIQ")
-	GraspitCommander.importGraspableBody("/home/asl-7/graspit/models/objects/cylinder.xml", pose)
+	# Combined import file of table and gripper.
+	GraspitCommander.loadWorld("grasp")
+	# GraspitCommander.importRobot("RobotIQ")
+	
+	GraspitCommander.importGraspableBody("/home/asl-7/Downloads/Part1(m).STL", object_pose)
+	# GraspitCommander.importGraspableBody("cylinder", object_pose)
+
 	rospy.sleep(0.1)
 
 	# Plan grasps
@@ -38,7 +44,8 @@ def grasp():
 
 	# The variable best_pose contains information of the best grasp like the pose of the gripper, approach direction and dofs of the joints.
 	best_pose = planned_grasps.grasps[arg_max]
-	transformed_pose = object_world_transformation(best_pose.pose, pose)
+	# Get the transforms of the gripper wrt the world coordinates.
+	transformed_pose = object_world_transformation(best_pose.pose, object_pose)
 
 	rospy.loginfo(transformed_pose)
 	rospy.loginfo(best_pose.dofs)	
