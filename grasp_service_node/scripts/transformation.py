f7f8d5e6f4e49ff65b655f26762456f6cf3ddba9
R-Charan <R-Charan>
2023-11-16 18:07:51 +0530

    Modified to maintain uniformity

--- grasp_service_node/scripts/transformation.py
+++ grasp_service_node/scripts/transformation.py
@@ -1,4 +1,4 @@
-#!usr/bin/env python
+#!/usr/bin/env python
 
 import rospy
 from tf.transformations import quaternion_matrix, quaternion_from_matrix
@@ -7,18 +7,28 @@ from geometry_msgs.msg import Pose
 
 def object_world_transformation(gripper_pose, object_pose):
     pos = []
-    gripper_rotation_matrix = quaternion_matrix(gripper_pose.orientation)
-    object_rotation_matrix = quaternion_matrix(object_pose.orientation)
+
+    # Obtain the 4x4 homogeneous rotation matrix without the translation information.
+    gripper_rotation_matrix = quaternion_matrix([gripper_pose.orientation.x,gripper_pose.orientation.y,gripper_pose.orientation.z,gripper_pose.orientation.w])
+    object_rotation_matrix = quaternion_matrix([object_pose.orientation.x, object_pose.orientation.y, object_pose.orientation.z, object_pose.orientation.w])
+
+    # Update the translational terms of both the homogeneous matrices
+    gripper_rotation_matrix[0,3] = gripper_pose.position.x
+    object_rotation_matrix[0,3] = object_pose.position.x
+    gripper_rotation_matrix[1,3] = gripper_pose.position.y
+    object_rotation_matrix[1,3] = object_pose.position.y
+    gripper_rotation_matrix[2,3] = gripper_pose.position.z
+    object_rotation_matrix[2,3] = object_pose.position.z
+
+    # Matrix Multiplying to get the final homogeneous transform
+    final_transformation = np.matmul(gripper_rotation_matrix, object_rotation_matrix)
+    # Finding the quaternions from the final matrix
+    quat = quaternion_from_matrix(final_transformation)
 
     for i in range(3):
-        gripper_rotation_matrix[i,3] = gripper_pose.position[i]
-        object_rotation_matrix[i,3] = object_pose.position[i]
+        pos.append(final_transformation[i,3])
     
-    final_transformation = np.matmul(gripper_rotation_matrix, object_rotation_matrix)
-    quat = quaternion_from_matrix(final_transformation)
-    for i in range(3):
-        pos[i] = final_transformation[i,3]
-    
+    # Adding all the calculated elements into the final_gripper_pose variable
     final_gripper_pose = Pose()
     final_gripper_pose.position.x = pos[0]
     final_gripper_pose.position.y = pos[1]
@@ -37,24 +47,21 @@ if __name__ == "__main__":
     object = Pose()
 
     gripper.position.x = 0
-    gripper.position.y = 0
-    gripper.position.z = 0
-    gripper.orientation.x = 0
+    gripper.position.y = 0.5
+    gripper.position.z = 0.5
+    gripper.orientation.x = 3.14
     gripper.orientation.y = 0
-    gripper.orientation.z = 0
+    gripper.orientation.z = 3.14
     gripper.orientation.w = 1
 
     object.position.x = 0.5
     object.position.y = 0
     object.position.z = 0
-    object.orientation.x = 0
-    object.orientation.y = 0
+    object.orientation.x = 1.57
+    object.orientation.y = 3.14
     object.orientation.z = 0
     object.orientation.w = 1
 
     world_gripper_pose = object_world_transformation(gripper, object)
     print(world_gripper_pose)
-
-
-
-
+    
--- gripper/scripts/transformation.py
+++ gripper/scripts/transformation.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+
 import rospy
 from tf.transformations import quaternion_matrix, quaternion_from_matrix
 import numpy as np
@@ -6,35 +7,28 @@ from geometry_msgs.msg import Pose
 
 def object_world_transformation(gripper_pose, object_pose):
     pos = []
-    # Isolating the quaternion angles alone into a numpy array.
-    gripper_quaternion = np.array([gripper_pose.orientation.x, gripper_pose.orientation.y, gripper_pose.orientation.z, gripper_pose.orientation.w])
-    object_quaternion = np.array([object_pose.orientation.x, object_pose.orientation.y, object_pose.orientation.z, object_pose.orientation.w])
-
-    # Isolating the positions into a numpy array.
-    gripper_position = np.array([gripper_pose.position.x, gripper_pose.position.y, gripper_pose.position.z])
-    object_position = np.array([object_pose.position.x, object_pose.position.y, object_pose.position.z])
 
     # Obtain the 4x4 homogeneous rotation matrix without the translation information.
-    gripper_rotation_matrix = quaternion_matrix(gripper_quaternion)
-    object_rotation_matrix = quaternion_matrix(object_quaternion)
+    gripper_rotation_matrix = quaternion_matrix([gripper_pose.orientation.x,gripper_pose.orientation.y,gripper_pose.orientation.z,gripper_pose.orientation.w])
+    object_rotation_matrix = quaternion_matrix([object_pose.orientation.x, object_pose.orientation.y, object_pose.orientation.z, object_pose.orientation.w])
 
-    # Adding the translation information to the homogeneous matrix.
-    for i in range(3):
-        gripper_rotation_matrix[i,3] = gripper_position[i]
-        object_rotation_matrix[i,3] = object_position[i]
-    
-    # Matrix multiplying to get the final transformation matrix of the gripper from the world frame.
+    # Update the translational terms of both the homogeneous matrices
+    gripper_rotation_matrix[0,3] = gripper_pose.position.x
+    object_rotation_matrix[0,3] = object_pose.position.x
+    gripper_rotation_matrix[1,3] = gripper_pose.position.y
+    object_rotation_matrix[1,3] = object_pose.position.y
+    gripper_rotation_matrix[2,3] = gripper_pose.position.z
+    object_rotation_matrix[2,3] = object_pose.position.z
+
+    # Matrix Multiplying to get the final homogeneous transform
     final_transformation = np.matmul(gripper_rotation_matrix, object_rotation_matrix)
-    # print(final_transformation)
-
-    # Finding the quaternions from the homogeneous matrix
+    # Finding the quaternions from the final matrix
     quat = quaternion_from_matrix(final_transformation)
 
-    # Getting the translation information from the last column of the homogeneous matrix
     for i in range(3):
-        pos.append(final_transformation[i][3])
-
-    # Adding all the determined elements into the final_gripper_pose variable
+        pos.append(final_transformation[i,3])
+    
+    # Adding all the calculated elements into the final_gripper_pose variable
     final_gripper_pose = Pose()
     final_gripper_pose.position.x = pos[0]
     final_gripper_pose.position.y = pos[1]
@@ -44,32 +38,30 @@ def object_world_transformation(gripper_pose, object_pose):
     final_gripper_pose.orientation.z = quat[2]
     final_gripper_pose.orientation.w = quat[3]
 
+    rospy.loginfo(final_gripper_pose)
+
     return final_gripper_pose
 
 if __name__ == "__main__":
-
     gripper = Pose()
     object = Pose()
 
-    gripper.position.x = 0.5
-    gripper.position.y = 0
-    gripper.position.z = 0
-    gripper.orientation.x = 0
+    gripper.position.x = 0
+    gripper.position.y = 0.5
+    gripper.position.z = 0.5
+    gripper.orientation.x = 3.14
     gripper.orientation.y = 0
-    gripper.orientation.z = 0
+    gripper.orientation.z = 3.14
     gripper.orientation.w = 1
 
     object.position.x = 0.5
-    object.position.y = 0.5
+    object.position.y = 0
     object.position.z = 0
-    object.orientation.x = 0
-    object.orientation.y = 0
+    object.orientation.x = 1.57
+    object.orientation.y = 3.14
     object.orientation.z = 0
     object.orientation.w = 1
 
     world_gripper_pose = object_world_transformation(gripper, object)
     print(world_gripper_pose)
-
-
-
-
+    
